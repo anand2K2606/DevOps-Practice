b3fe79138bdb9b4d0fe4d3b381b222a560c12b5f
Anand <anand@example.com>
2026-03-27 23:24:53 +0530

    Initial commit of demo-api and project settings

---
+++ demo-api/src/main/java/com/devops/practice/demoapi/DemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.devops.practice.demoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApiApplication.class, args);
+    }
+
+}
